98ac468824c7436952d195f14b540301f7814b81
zrefind <zrefind@gmail.com>
2022-12-03 14:42:22 +0900

    결산 엔티티 작성: Settlement

---
+++ src/main/java/org/kang/assignment/domain/settlement/Settlement.java
@@ -0,0 +1,64 @@
+package org.kang.assignment.domain.settlement;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Settlement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @Column(nullable = false)
+    private LocalDateTime time;
+
+    @Column(nullable = false)
+    private Long newbie;
+
+    @Column(nullable = false)
+    private Long bolter;
+
+    @Column(nullable = false)
+    private Long payment;
+
+    @Column(nullable = false)
+    private Long used;
+
+    @Column(nullable = false)
+    private Long sales;
+
+    private Settlement(LocalDateTime time, Long newbie, Long bolter, Long payment, Long used, Long sales) {
+        this.time = time;
+        this.newbie = newbie;
+        this.bolter = bolter;
+        this.payment = payment;
+        this.used = used;
+        this.sales = sales;
+    }
+
+    public static Settlement of(String time, String newbie, String bolter, String payment, String used, String sales) {
+        return new Settlement(
+                LocalDateTime.parse(time, DateTimeFormatter.ofPattern("yyyy-MM-dd HH")),
+                Long.parseLong(newbie.replace(",", "")),
+                Long.parseLong(bolter.replace(",", "")),
+                Long.parseLong(payment.replace(",", "")),
+                Long.parseLong(used.replace(",", "")),
+                Long.parseLong(sales.replace(",", ""))
+        );
+    }
+
+}
---
+++ src/main/java/org/kang/assignment/domain/settlement/SettlementRepository.java
@@ -0,0 +1,6 @@
+package org.kang.assignment.domain.settlement;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SettlementRepository extends JpaRepository<Settlement, Long> {
+}
