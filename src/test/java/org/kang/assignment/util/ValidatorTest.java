ae30d5b6867b3d9bf3d32230d1d92fc6e2017f36
zrefind <zrefind@gmail.com>
2022-12-05 20:41:15 +0900

    입력값 검증 유틸 테스트

--- src/test/java/org/kang/assignment/util/ValidatorTest.java
+++ src/test/java/org/kang/assignment/util/ValidatorTest.java
@@ -10,14 +10,72 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ValidatorTest {
 
     @Test
+    @DisplayName("시간_입력값_검증")
+    public void validateTime() {
+        String valid = "2022120100";
+
+        Validator.validateTime(valid);
+    }
+
+    @Test
+    @DisplayName("시간_입력값_검증__문자열_길이_오류")
+    public void validateTime__invalid_length() {
+        String invalidLength = "202212010";
+
+        try {
+            Validator.validateTime(invalidLength);
+        } catch (CustomException e) {
+            assertEquals(ErrorCode.INVALID_TIME, e.getErrorCode());
+        }
+    }
+
+    @Test
+    @DisplayName("시간_입력값_검증__월_오류")
+    public void validateTime__invalid_month() {
+        String invalidMonth = "2022130000";
+
+        try {
+            Validator.validateTime(invalidMonth);
+        } catch (CustomException e) {
+            assertEquals(ErrorCode.INVALID_TIME, e.getErrorCode());
+        }
+    }
+
+    @Test
+    @DisplayName("시간_입력값_검증__일_오류")
+    public void validateTime__invalid_day() {
+        String invalidDay = "2022123200";
+
+        try {
+            Validator.validateTime(invalidDay);
+        } catch (CustomException e) {
+            assertEquals(ErrorCode.INVALID_TIME, e.getErrorCode());
+        }
+    }
+
+    @Test
+    @DisplayName("시간_입력값_검증__시간_오류")
+    public void validateTime__invalid_hour() {
+        String invalidHour = "2022120124";
+
+        try {
+            Validator.validateTime(invalidHour);
+        } catch (CustomException e) {
+            assertEquals(ErrorCode.INVALID_TIME, e.getErrorCode());
+        }
+    }
+
+    @Test
     @DisplayName("기간_검증")
     public void validatePeriod() {
-        Validator.validatePeriod("2022120100", "2022120110");
+        String from = "2022120100";
+        String to = "2022120110";
+        Validator.validatePeriod(from, to);
     }
 
     @Test
     @DisplayName("기간_검증__시작값_없음")
-    public void validatePeriod_invalid_from() {
+    public void validatePeriod_missing_from() {
         try {
             Validator.validatePeriod("", "2022120110");
         } catch (CustomException e) {
@@ -27,7 +85,7 @@ public class ValidatorTest {
 
     @Test
     @DisplayName("기간_검증__종료값_없음")
-    public void validatePeriod_invalid_to() {
+    public void validatePeriod_missing_to() {
         try {
             Validator.validatePeriod("2022120100", "");
         } catch (CustomException e) {
@@ -36,18 +94,8 @@ public class ValidatorTest {
     }
 
     @Test
-    @DisplayName("기간_검증__입력값_오류")
-    public void validatePeriod__invalid_input() {
-        try {
-            Validator.validatePeriod("123", "2022120110");
-        } catch (CustomException e) {
-            assertEquals(ErrorCode.INVALID_PERIOD, e.getErrorCode());
-        }
-    }
-
-    @Test
     @DisplayName("기간_검증__시작이_종료보다_큼")
-    public void validatePeriod__from_is_after_to() {
+    public void validatePeriod__invalid_period() {
         try {
             Validator.validatePeriod("2022120110", "2022120100");
         } catch (CustomException e) {
@@ -110,6 +158,19 @@ public class ValidatorTest {
             Validator.validatePassword(password04);
         } catch (CustomException e) {
             assertEquals(ErrorCode.INVALID_PASSWORD, e.getErrorCode());
+        }
+    }
+
+    @Test
+    @DisplayName("정수값의_양수_판별")
+    public void ifNonNullValidatePositive() {
+        Validator.ifNonNullValidatePositive(0L, 1L, 2L);
+        Validator.ifNonNullValidatePositive(null, 1L, 2L);
+
+        try {
+            Validator.ifNonNullValidatePositive(0L, -1L, 2L);
+        } catch (CustomException e) {
+            assertEquals(ErrorCode.NOT_POSITIVE, e.getErrorCode());
         }
     }
 
